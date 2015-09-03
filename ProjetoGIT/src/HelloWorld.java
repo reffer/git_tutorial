b6156408b537ce73162f65709c57a0f9bd6970a6
Andre Lima <andre@reffer.com.br>
2015-09-03 13:00:16 -0300

    Adicionado out

--- ProjetoGIT/src/HelloWorld.java
+++ ProjetoGIT/src/HelloWorld.java
@@ -1,11 +1,15 @@
 
 public class HelloWorld {
 	
+	private static void out(String str){
+		System.out.println(str);
+	}
+	
 	public static void main(String[] args) {
 		
 		// comentario
-		System.out.println("Hello World");
-		
+		// System.out.println("Hello World");
+		out("Hello World");
 	}
 
 }
